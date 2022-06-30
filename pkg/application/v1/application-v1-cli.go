9acc5de873fbbd597cc62d08d24b64363da1190b
Tama MA <tamama@tamama.io>
2022-06-30 08:51:20 +0100

    feat/london-7: Added version

--- pkg/application/v1/application-v1-cli.go
+++ pkg/application/v1/application-v1-cli.go
@@ -1,9 +1,15 @@
 package applicationv1
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/tamama/tamama-cloud-deployer-machine/resource"
 	"github.com/urfave/cli/v2"
+)
+
+var (
+	flag_version *bool
 )
 
 func (_self *ApplicationV1) _NewApp() (app *cli.App) {
@@ -17,7 +23,15 @@ func (_self *ApplicationV1) _NewApp() (app *cli.App) {
 				Email: "tamama@tamama.io",
 			},
 		},
-		Flags: []cli.Flag{},
+		Flags: []cli.Flag{
+			&cli.BoolFlag{
+				Name:        "version",
+				Aliases:     []string{"v"},
+				Value:       false,
+				Usage:       "print version",
+				Destination: flag_version,
+			},
+		},
 		Action: _self._RunAction,
 	}
 
@@ -30,9 +44,16 @@ func (_self *ApplicationV1) _NewApp() (app *cli.App) {
 
 func (_self *ApplicationV1) _RunAction(ctx *cli.Context) (err error) {
 	switch {
+	case ctx.Bool("version"):
+		err = _self._RunAction_Version(ctx)
 	default:
 		cli.ShowAppHelp(ctx)
 	}
 	os.Exit(0)
 	return err
+}
+
+func (_self *ApplicationV1) _RunAction_Version(ctx *cli.Context) (err error) {
+	fmt.Printf("tamama-cloud-deployer-machine version %s\n", resource.Version)
+	return nil
 }
